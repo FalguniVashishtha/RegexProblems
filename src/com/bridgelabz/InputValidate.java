eaa752f52340d590b083045ae0f837c61d5a50b2
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-18 23:56:57 +0530

    enter valid last name

---
+++ src/com/bridgelabz/InputValidate.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class InputValidate
+{
+    // Creating isValidFirstName method to validate the first name given by user using regex
+    //return - true or false
+    public static boolean isValidFirstName(String firstName) {
+
+        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";  //Declaring regex pattern to check the firstName
+
+        Pattern pattern = Pattern.compile(firstNameRegex);
+        Matcher matcher = pattern.matcher(firstName);
+
+        return matcher.matches();
+    }
+
+    // Creating isValidLastName method to validate the last name given by user using regex
+    public static boolean isValidLastName(String lastName) {
+        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
+
+        Pattern pattern = Pattern.compile(firstNameRegex);
+
+        Matcher matcher = pattern.matcher(lastName);
+
+        return matcher.matches();
+    }
+}
