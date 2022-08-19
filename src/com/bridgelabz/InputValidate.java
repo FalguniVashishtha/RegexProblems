6b8c85925967c0c3c64f8b3a57f18e2a74ac15d9
Falguni Vashishtha <shrutisharma2302@gmail.com>
2022-08-19 23:04:22 +0530

    validate all user email samples

--- src/com/bridgelabz/InputValidate.java
+++ src/com/bridgelabz/InputValidate.java
@@ -1,55 +1,24 @@
 package com.bridgelabz;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.Scanner;
 public class InputValidate {
-    // Creating isValidFirstName method to validate the first name given by user using regex
-    //return - true or false
-    public static boolean isValidFirstName(String firstName) {
+    static Scanner sc = new Scanner(System.in);
 
-        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";  //Declaring regex pattern to check the firstName
-
-        Pattern pattern = Pattern.compile(firstNameRegex);
-        Matcher matcher = pattern.matcher(firstName);
-
-        return matcher.matches();
-    }
-
-    // Creating isValidLastName method to validate the last name given by user using regex
-    public static boolean isValidLastName(String lastName) {
-        String firstNameRegex = "[A-Z]{1}[a-z]{2,}";
-
-        Pattern pattern = Pattern.compile(firstNameRegex);
-
-        Matcher matcher = pattern.matcher(lastName);
-
-        return matcher.matches();
-    }
-
-    public static boolean isValidEmail(String email) {
-
-        String emailRegex = "^[a-z]{3}[a-zA-Z.]{0,}+@+bl.co+[a-z.]{0,}$";
-
-        Pattern pattern = Pattern.compile(emailRegex);
-
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-
-    //Creating isValidMobileNumber method to validate the mobile format given by user using regex
-    public static boolean isValidMobileNumber(String mobileNumber) {
-        String mobileNumberRegex = "^[0-9]{2}[\\s][0-9]{10}$";
-        Pattern pattern = Pattern.compile(mobileNumberRegex);
-        Matcher matcher = pattern.matcher(mobileNumber);
-        return matcher.matches();
-    }
-
-    public static boolean validPassword(String password)
-    {
-        System.out.print("Enter the Password: ");
-        String passwordregex = "^[A-Z]{1}[0-9]{1}(?=.*[@$!%*#?&])[A-Za-z0-9@._-]{6,}$";
-        Pattern pattern = Pattern.compile(passwordregex);
-        Matcher matcher = pattern.matcher(password);
-        return matcher.matches();
+    // Method to check email.
+    public static boolean validEmailAddress() {
+        System.out.print("Enter the Email Address:  ");
+        String emailAddress = sc.next();
+        String regex = "^(?!.*@.*@)[a-z+_-]+(.[a-z0-9])*@[a-z0-9]+(.[a-z])*.[a-z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(emailAddress);
+        boolean result = m.matches();
+        // if condition is to check the email address is valid or not
+        if (result)
+            System.out.println("Your Email Address is valid");
+        else
+            System.out.println("Your Email Address is invalid");
+        return result;
     }
 }
 
--- src/com/bridgelabz/UserName.java
+++ src/com/bridgelabz/UserName.java
@@ -4,31 +4,28 @@ import java.util.regex.Matcher;
 import java.util.Scanner;
 
 public class UserName {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("Welcome to User Registration problems.");
 
+        //creating object
         InputValidate obj = new InputValidate();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter first name: ");
-        String firstName = scanner.next();
-        System.out.println(obj.isValidFirstName(firstName) ? "Valid" : "In Valid");
-        System.out.println("Enter last name: ");
-        String lastName = scanner.next();
-
-        //scanner.close();
-
-        System.out.println(obj.isValidLastName(lastName) ? "Valid" : "In Valid");
-
-        System.out.println("Enter Email: ");
-        String email = scanner.next();
-        System.out.println(obj.isValidEmail(email) ? "Valid" : "In Valid");
-
-        System.out.println("Enter Mobilenumber: ");
-        String mobileNumber = scanner.next();
-        System.out.println(obj.isValidMobileNumber(mobileNumber) ? "Valid" : "In Valid");
-
-        System.out.println("Enter password: ");
-        String password = scanner.next();
-        System.out.println(obj.validPassword(password) ? "Valid" : "In Valid");
+        int choose;
+        int select;
+        do {
+            System.out.println("Press 1: Email Address Validation\n2. Exit");
+            choose = sc.nextInt();
+            switch (choose) {
+                case 1:
+                    obj.validEmailAddress();
+                    break;
+                default:
+                    System.out.println("Invalid: Enter valid details.");
+                    break;
+            }
+            System.out.println("If you want to continue press 1 or to exit press 2");
+            select = sc.nextInt();
+        } while (select != 2);
     }
 
 }
